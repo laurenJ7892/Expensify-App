baf0fe71766978d57d4943ccab6224f2cb8d8fb4
Lauren <lauren@powr.io>
2020-09-28 20:40:07 -0700

    updating removing default senses

--- src/app.js
+++ src/app.js
@@ -14,16 +14,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 250 }));
-store.dispatch(addExpense({ description: 'Gas Bill', createdAt: 1000}));
-store.dispatch(addExpense({ description: 'Rent bill', amount: 10250 }));
-
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
