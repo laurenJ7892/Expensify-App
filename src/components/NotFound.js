e88e67beecee0f752f3aec97c44f2dedd868b6df
Lauren <lauren@powr.io>
2020-10-04 14:12:16 -0700

    debugging

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => {
-  <div>
-    This is from my not found page 
-    <Link to="/">Go Home</Link>
-  </div>
+  return (
+    <div>
+      <Link to="/">Feeling Lost? Let's go Home</Link>
+    </div>
+  )
 };
 
 export default NotFoundPage;
