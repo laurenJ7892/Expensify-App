1be02507c4279f606cb823b1846a04d7189ebf62
Lauren <lauren@powr.io>
2020-10-04 19:14:42 -0700

    debugging package versions

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from '../routers/AppRouter';
 import configStore from '../src/store/configureStore';
-import { addExpense } from '../src/actions/expenses';
-import { setTextFilter } from '../src/actions/filters';
-import getVisibleExpenses from '../src/Selectors/expenses';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
