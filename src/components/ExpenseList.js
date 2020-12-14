881a05ea8704580ea98aafc24af9adcdef0681f8
Lauren <lauren@powr.io>
2020-12-13 23:39:54 -0800

    design updates

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -5,17 +5,24 @@ import selectExpenses from '../Selectors/expenses';
 
 export const ExpenseList = (props) => {
   return (
-    <div>
-       <h1>Expense List</h1>
-      {
-        props.expenses.length === 0 ? (
-          <p>No expenses</p>
-        ) : (
-          props.expenses.map((expense) => {
-            return <ExpenseListItem key={expense.id} {...expense} />;
-          })
-        )
-      }
+    <div className='content-container'>
+      <div className="list--header">
+        <div className="show-for-mobile">Expenses</div>
+        <div className="show-for-desktop">Expense</div>
+        <div className="show-for-desktop">Amount</div>
+      </div>
+      <div className="list-body">
+        {props.expenses.length === 0 ? (
+          <div className="list--item list-item--message">
+            <span>No expenses</span>
+          </div>
+          ) : (
+            props.expenses.map((expense) => {
+              return <ExpenseListItem key={expense.id} {...expense} />;
+            })
+          )
+        }
+      </div>
     </div>
   );
 }; 
